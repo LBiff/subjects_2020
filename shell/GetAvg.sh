5fd411317d466b22168797be889516394f5a108d
LBiff <15004635770@126.com>
2020-11-05 16:21:33 +0800

    update

--- shell/GetAvg.sh
+++ shell/GetAvg.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+# 写一个bash脚本以实现一个需求，求输入的一个的数组的平均值
+
+# 第1行为输入的数组长度N
+# 第2~N行为数组的元素，如以下为:
+
+read N
+S=0
+C=0
+while [ $C -lt $N ]; do
+    read A
+    S=$(($S+$A))
+    C=$(($C+1))
+done
+ 
+printf "%.3f" $(echo "$S / $N" | bc -l)
+
+
+
